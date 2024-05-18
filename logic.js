903631f4533f1448d959977f895797fd7764274a
Parchello <parchellos@gmail.com>
2024-05-18 14:59:26 +0300

    start

---
+++ logic.js
@@ -0,0 +1,15 @@
+// document.getElementById("loading").style.display = "none";
+// document.getElementById("bigContainer").style.display = "none";
+// document.getElementById("valueCont").style.display = "none";
+// document.getElementById("userCont").style.display = "none";
+// document.getElementById("opponentCont").style.display = "none";
+// document.getElementById("whosTurn").style.display = "none";
+
+// const socket = io();
+
+// document.getElementById("find").addEventListener("click", function () {
+//   let name = "";
+//   console.log("ghghgh");
+//   name = document.getElementById("name").value;
+//   console.log(name);
+// });
